702d656ddbcf2b2609372a7c91cd12650c0f89d6
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-05-15 11:27:54 -0600

    started Employee object and testing

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+const { test, expect } = require("@jest/globals");
+const Employee = require("../lib/Employee");
+
+test("creates an employee object", () => {
+    const employee = new Employee("Casey");
+
+    expect(employee.name).toBe("Casey");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
